e21b6b693bbdb47dde1c6cee162e1c4c3301c406
Marlon Strong <tsenane14@gmail.com>
2021-01-22 21:50:02 +0200

    first commit

---
+++ gatsby-example/hello-world/src/pages/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Home() {
+  return (
+    <div style={{ color: `purple` }}>
+      <h1>Hello Gatsby!</h1>
+      <p>What a world.</p>
+    </div>
+  );
+}
